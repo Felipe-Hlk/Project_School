c33d31f96fbb39666658a515992f3570b060b7ab
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-08-07 18:53:53 -0300

    styling users comp

--- project-school/src/app/shared/materials/users-mat.module.ts
+++ project-school/src/app/shared/materials/users-mat.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @NgModule({
 
   exports: [
-    MatTabsModule
+    MatTabsModule,
+    MatIconModule
 
   ],
   declarations:[],
