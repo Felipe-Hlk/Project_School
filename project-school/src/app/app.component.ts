27be0e86f4d5128af3d480b0d64ea463c231b18e
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-08-07 16:10:46 -0300

    config imports and update app.component.ts

--- project-school/src/app/app.component.ts
+++ project-school/src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import {BreakpointObserver} from '@angular/cdk/layout'
+import { fromEvent, map } from 'rxjs';
+
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'project-school';
+export class AppComponent implements OnInit {
+
+  public smallScreen = false;
+  public popText = false;
+  public applyShadow = false;
+
+  constructor(private breakpointObserver: BreakpointObserver){ }
+  ngOnInit(): void {
+    const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+    console.log (content);
+
+    fromEvent (content, 'scroll')
+    .pipe(
+      map(() => content.scrollTop)
+    )
+    .subscribe({
+      next: (value: number) => this.determineHeader(value)
+    })
+  }
+
+  determineHeader (scrollHeight: number){
+    console.log(scrollHeight)
+    this.popText = scrollHeight >= TEXT_LIMIT;
+    this.applyShadow = scrollHeight >= SHADOW_LIMIT;
+  }
+
+
+
+  ngAfterContentInit(): void{
+    this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
+      next: (res) => {                 //res = retorno
+        if (res.matches){
+          this.smallScreen = true;
+        }else{
+          this.smallScreen = false;
+        }
+      }
+    })
+  }
+
+  get sidenavMode() {
+    return this.smallScreen ? 'over' : 'side';
+  }
+
 }
--- project-school/src/app/app.module.ts
+++ project-school/src/app/app.module.ts
@@ -5,31 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './material/material.module';
-import { ButtonsIconsComponent } from './buttons-icons/buttons-icons.component';
-import { FormFieldInputComponent } from './form-field-input/form-field-input.component';
+import { MaterialModule } from './shared/materials/material.module';
+
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { ProgressSpinnerComponent } from './progress-spinner/progress-spinner.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { DatepickerComponent } from './datepicker/datepicker.component';
-import { SnackbarComponent } from './snackbar/snackbar.component';
-import { DialogComponent } from './dialog/dialog.component';
-import { DialogModalComponent } from './dialog/dialog-modal/dialog-modal.component';
-import { DatatableComponent } from './datatable/datatable.component';
+
+// import { ButtonsIconsComponent } from './buttons-icons/buttons-icons.component';
+// import { FormFieldInputComponent } from './form-field-input/form-field-input.component';
+// import { ProgressSpinnerComponent } from './progress-spinner/progress-spinner.component';
+// import { TabsComponent } from './tabs/tabs.component';
+// import { DatepickerComponent } from './datepicker/datepicker.component';
+// import { SnackbarComponent } from './snackbar/snackbar.component';
+// import { DialogComponent } from './dialog/dialog.component';
+// import { DialogModalComponent } from './dialog/dialog-modal/dialog-modal.component';
+// import { DatatableComponent } from './datatable/datatable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsIconsComponent,
-    FormFieldInputComponent,
-    ProgressSpinnerComponent,
-    TabsComponent,
-    DatepickerComponent,
-    SnackbarComponent,
-    DialogComponent,
-    DialogModalComponent,
-    DatatableComponent
+
   ],
   imports: [
     BrowserModule,
@@ -42,3 +36,16 @@ import { DatatableComponent } from './datatable/datatable.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/**
+ * ButtonsIconsComponent,
+    FormFieldInputComponent,
+    ProgressSpinnerComponent,
+    TabsComponent,
+    DatepickerComponent,
+    SnackbarComponent,
+    DialogComponent,
+    DialogModalComponent,
+    DatatableComponent
+ */
