b2b8278490a04b305372adbaf9b4b41afba10c0a
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-08-08 17:33:46 -0300

    create toolbar tile component in shared folder

---
+++ project-school/src/app/shared/components/toolbar-title/toolbar-title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-toolbar-title',
+  templateUrl: './toolbar-title.component.html',
+  styleUrls: ['./toolbar-title.component.scss']
+})
+export class ToolbarTitleComponent {
+
+}
