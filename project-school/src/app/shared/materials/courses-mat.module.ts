7128b1e6f613a76d405f4ff45012eea8cef99ca7
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-08-09 17:35:59 -0300

    Create courses-mat.module.ts

---
+++ project-school/src/app/shared/materials/courses-mat.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+
+
+@NgModule({
+
+  exports: [
+    MatTabsModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  declarations:[],
+  providers:[],
+  imports: [],
+})
+
+export class CoursesMaterialModule { }
